975f2f263e9e0b5e1f3e764985609ac7152c03ac
Babang Putra Parhusip <parhusip@imt.ch>
2020-05-02 20:01:29 +0800

    Usage of multi threadings and clean up on qevent.py

--- tools/qevents.py
+++ tools/qevents.py
@@ -5,116 +5,102 @@
 To install sseclient:
     pip3 install sseclient
 """
-import requests
 import argparse
 import sys
 import time
 import json
 import base64
 import threading
-
 from sseclient import SSEClient
 
 lock = threading.Lock()
 
 # dictionary of device name to device id
-devid = {
-        'electron-0009': '430061000451353431383736',
-        'electron-0011': '330031000d47373336373936',
-        'electron-0017': '1f0035000b47373336373936',
-        'electron-0020': '28001f000747373334363431',
-        'electron-0016': '300026000247373334363431',
-        'electron-0014': '1f002e000647373336373936',
-        'electron-0015': '39002b000e47373334363431',
-        'electron-0012': '230038000f47373334363431',
-        'electron-0019': '4a003f001750483553353920',
-        'electron-0013': '230029000147373336373936',
-        }
+DEVID = {
+    'electron-0009': '430061000451353431383736',
+    'electron-0011': '330031000d47373336373936',
+    'electron-0017': '1f0035000b47373336373936',
+    'electron-0020': '28001f000747373334363431',
+    'electron-0016': '300026000247373334363431',
+    'electron-0014': '1f002e000647373336373936',
+    'electron-0015': '39002b000e47373334363431',
+    'electron-0012': '230038000f47373334363431',
+    'electron-0019': '4a003f001750483553353920',
+    'electron-0013': '230029000147373336373936',
+    }
 
-events_cnt = {
-        'v1-observation-values':0,
-        'v1-observation-settings':0,
-        'v1-device-meta':0,
-        'v1-patient-meta':0,
-        }
-decoded_data=""
+EVENTS_CNT = {
+    'v1-observation-values':0,
+    'v1-observation-settings':0,
+    'v1-device-meta':0,
+    'v1-patient-meta':0,
+    }
 
 def get_args():
     """Parse the input argument."""
     parser = argparse.ArgumentParser()
-
     parser.add_argument("-n", "--devname", required=True,
-           help="device name e.g. electron-0009")
+                        help="device name e.g. electron-0009")
     parser.add_argument("-d", "--duration", required=True,
-           type=int, help="duration in secs to collect data")
+                        type=int, help="duration in secs to collect data")
     parser.add_argument("-t", "--token", required=True,
-           help="token for GET http request")
-
+                        help="token for GET http request")
     return parser.parse_args()
 
-def decode(event):
+def decode(data):
     """To decode base64 encoded data."""
-    # extract
-    data_base64 = json.loads(event.data)['data']
-    coreid = json.loads(event.data)['coreid']
-    # TODO to decode base64 data
-    print(event)
-    global decoded_data
-    decoded_data = base64.b64decode(data_base64)
+    decoded_data = base64.b64decode(data)
     # still can understand the data
     # ref: you can checkout the method in DecodeV1ObservationValues
     # in MessageDecoder.cs
-    print(base64.b64decode(data_base64))
+    return decoded_data
 
 def is_found(devicename):
     """To search if given device found in the valid device names list."""
-    for dev in list(devid):
-        if (dev == devicename):
+    for dev in list(DEVID):
+        if dev == devicename:
             return True
-
     return False
 
 def print_valid_devices():
     """Tell user which devices are supported."""
     print("valid device names")
-    for dev in list(devid):
+    for dev in list(DEVID):
         print(dev)
 
-def get_stream(event_type):
+def get_stream(event_name):
     """To get the stream events object."""
-    url =  "https://api.particle.io/v1/events/"+event_type
+    url = "https://api.particle.io/v1/events/"+event_name
     headers = {'Authorization': 'Bearer '+args.token}
-
-    events = SSEClient(url, headers = headers)
     start_time = int(time.time())
-    for event in events:
-        if event.data.count('data') > 0:
-            coreid = json.loads(event.data)['coreid']
-            if coreid.strip() == devid[args.devname].strip():
-                lock.acquire()
-                events_cnt[event_type] += 1
-                lock.release()
-                decode(event)
 
-        if((int(time.time()) - start_time) > args.duration):
+    events = SSEClient(url, headers=headers)
+    for curr_event in events:
+        if curr_event.data.count('data') > 0:
+            coreid = json.loads(curr_event.data)['coreid']
+            if coreid.strip() == DEVID[args.devname].strip():
+                lock.acquire()
+                EVENTS_CNT[event_name] += 1
+                lock.release()
+                data_base64 = json.loads(curr_event.data)['data']
+                decode(data_base64)
+                print("%s\n%s\n%s\n" %(event_name, curr_event, decode(data_base64)))
+
+        if(int(time.time()) - start_time) > args.duration:
             break
 
 if __name__ == '__main__':
     args = get_args()
 
-    if is_found(args.devname) == False :
+    if is_found(args.devname) is False:
         print("device not found!")
         print_valid_devices()
         sys.exit(1)
 
-    # TODO: to query for all events, for now just one
-    # Maybe can leverage python multi threading feature?
-    #event_type = 'v1-observation-values'
-    #get_stream(event_type)
     threads = []
-    for event_type in list(events_cnt):
-        print("Start thread for %s" %(event_type))
-        threads.append(threading.Thread(target=get_stream,args=(event_type,)))
+
+    for event in list(EVENTS_CNT):
+        threads.append(threading.Thread(target=get_stream, args=(event,)))
 
     for thread in threads:
         thread.start()
@@ -122,9 +108,7 @@ if __name__ == '__main__':
     for thread in threads:
         thread.join()
 
-
     # Summary
     print("count: event name")
-    for event in list(events_cnt):
-        print("%d: %s" %(events_cnt[event], event))
-
+    for event in list(EVENTS_CNT):
+        print("%d: %s" %(EVENTS_CNT[event], event))
