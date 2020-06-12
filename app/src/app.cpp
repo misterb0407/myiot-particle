9004fa0f651a49443e8a7e3fc7c0a11797550471
Babang Putra Parhusip <b4b4ng@gmail.com>
2020-06-12 22:53:01 +0800

    clean up

--- app/src/app.cpp
+++ app/src/app.cpp
@@ -1,32 +1,21 @@
 #include "platform.h"
 
-bool flag = false;
-int analogvalue = 0;
-double tempC = 0;
-String message = "my name is Babang Putra";
-String aString;
+class App {
+    public:
+        App(){}
+        static int32_t echo(String input) {
+            Log.info(input);
+            return 0;
+        }
+};
 
 
 void setup() {
     API.init();
-    Particle.variable("flag", flag);
-    Particle.variable("analogvalue", analogvalue);
-    Particle.variable("temp", tempC);
-    if(Particle.variable("mess", message) == false)
-    {
-        //variable not registered!
-    }
-    Particle.variable("mess2", aString);
-
-    pinMode(A0, INPUT);
+    Particle.function("echo", App::echo);
 
 }
 
 void loop() {
-    //Read the analog value of the sensor TMP36
-    analogvalue = analogRead(A0);
-    //convert the reading into degree Celcius
-    tempC = (((analogvalue * 3.3) / 4095) - 0.5) *100;
-    Log.info("testing %lf", tempC);
     delay(2000);
 }
