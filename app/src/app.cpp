c5e833bdba91db3897e24b347c0028e294b9a86c
Babang Putra Parhusip <b4b4ng@gmail.com>
2020-10-16 14:39:19 +0800

    Add temperature sensor from starter kit

--- app/src/app.cpp
+++ app/src/app.cpp
@@ -1,4 +1,8 @@
+#include "Grove_Temperature_And_Humidity_Sensor.h"
+
 #include "platform.h"
+
+DHT dht(D2);
 
 void eventHandler(system_event_t event, int param) {
     uint32_t lsb_event = static_cast<uint32_t>(event);
@@ -8,7 +12,18 @@ void eventHandler(system_event_t event, int param) {
 void setup() {
     API.init();
     System.on(all_events, &eventHandler);
+    dht.begin();
 }
 
 
-void loop() { delay(2000); }
+void loop() {
+    float temp, humidity;
+
+    temp = dht.getTempFarenheit();
+    humidity = dht.getHumidity();
+
+    Serial.printlnf("Temp: %f", temp);
+    Serial.printlnf("Humidity: %f", humidity);
+
+    delay(10000);delay(2000); 
+}
